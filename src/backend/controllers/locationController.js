672284aadf076f897ef3afbc12feab04c59eae48
tuckerdev <tuckerdev100@outlook.com>
2023-10-23 03:43:57 -0600

    all tests for first controller passing

--- src/backend/controllers/locationController.js
+++ src/backend/controllers/locationController.js
@@ -22,12 +22,14 @@ const getLocation = async (req, res) => {
         longitude,
       };
 
-      res.json(jsonResponse);
-    } else {
-      res.status(404).json({ error: "Location Not Found" });
-    }
+      res.status(200).json(jsonResponse);
+    } 
   } catch (err) {
-    res.status(500).json({ error: "Internal Server Error" });
+    if (err.response && err.response.status === 404) {
+      res.status(404).json({ error: "Location Not Found" });
+    } else {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
 };
 
--- src/backend/test/UnitTests/locationController.spec.js
+++ src/backend/test/UnitTests/locationController.spec.js
@@ -1,67 +1,94 @@
 const chai = require('chai');
 const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
 const axios = require('axios');
 const { expect } = chai;
 
-chai.use(sinonChai);
+chai.should();
 
-// Import the controller function you want to test
+// Import your controller function
 const locationController = require('../../controllers/locationController');
 
-describe('Location Controller', () => {
-  it('should return location data when the API call is successful', async () => {
-    // Create a sandbox for spies/stubs
-    const sandbox = sinon.createSandbox();
+const geoCodeHappyPath = require('../Mocks/geoCodeHappyPath.json')
 
-    // Stub the axios.get method to return a predefined response
-    sandbox.stub(axios, 'get').resolves({ data: 'Mocked Location Data' });
 
-    // Create mock request and response objects
-    const req = {
-      params: { state: 'CA', zip: '90210', city: 'BeverlyHills' },
-    };
 
-    const res = {
-      send: (data) => {
-        expect(data).to.deep.equal({ result: 'Mocked Location Data' });
-      },
-    };
+describe('locationController', () => {
+  describe('getLocation', () => {
+    it('should return location data when a valid zip code is provided', async () => {
+      const validZip = '80303';
 
-    // Call the controller function
-    await locationController.getLocation(req, res);
+      // Stub the axios.get method to return a mock response
+      const axiosStub = sinon.stub(axios, 'get');
+      axiosStub.returns(Promise.resolve({ data: geoCodeHappyPath }));
 
-    // Restore axios.get to its original state
-    sandbox.restore();
-  });
+      const req = { params: { zip: validZip } };
+      const res = {
+        status: (statusCode) => {
+          statusCode.should.equal(200);
+          return {
+            json: (data) => {
+              // Assertions for the response JSON data
+              data.should.have.property('latitude');
+              data.should.have.property('longitude');
+            },
+          };
+        },
+      };
 
-  it('should handle errors gracefully when the API call fails', async () => {
-    // Create a sandbox for spies/stubs
-    const sandbox = sinon.createSandbox();
+      await locationController.getLocation(req, res);
 
-    // Stub the axios.get method to simulate an error
-    sandbox.stub(axios, 'get').rejects(new Error('Mocked Error'));
+      // Restore the axios.get method
+      axiosStub.restore();
+    });
 
-    // Create mock request and response objects
-    const req = {
-      params: { state: 'CA', zip: '90210', city: 'BeverlyHills' },
-    };
+    it('should return a "Location Not Found" error for an invalid zip code', async () => {
+      const invalidZip = 'invalidZip';
+    
+      // Stub the axios.get method to return a mock response with status 404
+      const axiosStub = sinon.stub(axios, 'get');
+      axiosStub.returns(Promise.resolve({ status: 404, data: {} }));
+    
+      const req = { params: { zip: invalidZip } };
+      const res = {
+        status: (statusCode) => {
+          statusCode.should.equal(404);
+          return {
+            json: (data) => {
+              data.should.have.property('error', 'Location Not Found');
+            },
+          };
+        },
+      };
+    
+      await locationController.getLocation(req, res);
+    
+      axiosStub.restore();
+    });
 
-    const res = {
-      status: (statusCode) => {
-        expect(statusCode).to.equal(404);
-        return {
-          json: (data) => {
-            expect(data).to.deep.equal({ error: 'Location Not Found' });
-          },
-        };
-      },
-    };
+    it('should handle internal server errors gracefully', async () => {
+      const invalidZip = '80303';
 
-    // Call the controller function
-    await locationController.getLocation(req, res);
+      // Stub the axios.get method to simulate an error
+      const axiosStub = sinon.stub(axios, 'get');
+      axiosStub.rejects(new Error('Internal Server Error'));
 
-    // Restore axios.get to its original state
-    sandbox.restore();
+      const req = { params: { zip: invalidZip } };
+      const res = {
+        status: (statusCode) => {
+          statusCode.should.equal(500);
+          return {
+            json: (data) => {
+              // Assertions for the error message
+              data.should.have.property('error', 'Internal Server Error');
+            },
+          };
+        },
+      };
+
+      await locationController.getLocation(req, res);
+
+      // Restore the axios.get method
+      axiosStub.restore();
+    });
   });
 });
